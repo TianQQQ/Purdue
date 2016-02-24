01a4e2d3878d4d72a8b86d6c63a8e26965c39f1f
Tian Qiu <qiu58@purdue.edu>
2016-02-24 17:47:10 -0500

    HW6 2 done

--- IE336_MarkovChain_Python3/HW6/HW6.py
+++ IE336_MarkovChain_Python3/HW6/HW6.py
@@ -1,16 +1,25 @@
 import numpy
-from pprint import pprint as pp
+from pprint import pprint
+import copy
 case1 = [[.1, .2, .3, .4], [.4, .3, .2, .1], [.5, .1, .1, .3], [.25, .25, .25, .25]]
-pp(case1)
-
 case1 = numpy.matrix(case1)
-print(case1)
-a = case1
-
-
-
-
+case2 = numpy.matrix(((.1, .2, .3, .4), (0, 1, 0, 0),(.5, .1,.1,.3),(.25,.25,.25,.25)))
+case3 = numpy.matrix(((0, .5, 0,.5),(.5,0,.5,0), (0,.4,0,.6),(.3,0,.7,0)))
+a = [1,1,1]
+for i in range(0,50):
+    a[0] *=case1
+print(a[0])
 
 for i in range(0,50):
-    a *=case1
-print(a)
+    a[1] *=case2
+print(a[1])
+
+for i in range(0,50):
+    a[2] *=case3
+print(a[2])
+
+
+'''
+case1 = copy.copy(case1)
+pprint(case1)
+'''
