7a749647b4ae75c57bc0c95bc9b4303341fb4fd0
Tian Qiu <qiu58@purdue.edu>
2016-03-02 22:58:59 -0500

    trying to get normal distribution and pdf line.....

--- STAT350_R/STAT350/Labs/Lab5/CItest.R
+++ STAT350_R/STAT350/Labs/Lab5/CItest.R
@@ -3,6 +3,29 @@
 #a) Make a boxplot and histogram to verify that the distribution is roughly symmetric with no outliers.
 DMS=read.table(file="DMS.txt",header=T)
 
+
+# histogram
+# View(DMS)
+# attach(DMS$DMS)
+# library(lattice) # histogram ??????lattice
+hist(DMS$DMS)
+means <- mean(DMS$DMS)
+std <- sd(DMS$DMS)
+curve(dnorm(x, mean=means, sd=std), col="blue", lwd=2, add=TRUE)   # normal distribution line
+lines(density(DMS$DMS, adjust=2),col = "red", lwd=2)
+
+# boxplot
+boxplot(DMS$DMS)
+points(means, pch = 18)
+
+
+
+
+
+
+
+
+
 # Parameters for t.test
 # You always indicate confidence level, alpha = 1 ??? C
 # possibilities for alternative are "two.sided" (confidence interval),
@@ -10,7 +33,4 @@ DMS=read.table(file="DMS.txt",header=T)
 t.test(DMS$DMS, conf.level=0.95, alternative = "two.sided")
 
 
-# boxplot
-View(DMS)
-attach(DMS)
-hist(DMS)
+
