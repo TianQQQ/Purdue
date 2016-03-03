6aced0447d53278cfe801ac7fcde90f990ec23f3
Tian Qiu <qiu58@purdue.edu>
2016-03-02 22:42:45 -0500

    t test for DMS done

---
+++ STAT350_R/STAT350/Labs/Lab5/CItest.R
@@ -0,0 +1,11 @@
+# for DMS
+
+#a) Make a boxplot and histogram to verify that the distribution is roughly symmetric with no outliers.
+DMS=read.table(file="DMS.txt",header=T)
+
+# Parameters for t.test
+# You always indicate confidence level, alpha = 1 ??? C
+# possibilities for alternative are "two.sided" (confidence interval),
+# "less" (upper confidence bound for one-sided test), "greater" (lower confidence bound for one-sided test)
+t.test(DMS$DMS, conf.level=0.95, alternative = "two.sided")
+
