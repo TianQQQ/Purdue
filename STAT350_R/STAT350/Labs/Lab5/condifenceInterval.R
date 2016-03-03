286d6ffe49936af324c0168aa26d59cabecc120e
Tian Qiu <qiu58@purdue.edu>
2016-03-03 01:07:32 -0500

    40 random coding done

--- STAT350_R/STAT350/Labs/Lab5/condifenceInterval.R
+++ STAT350_R/STAT350/Labs/Lab5/condifenceInterval.R
@@ -1,30 +1,56 @@
 # A
-SRS <- 1000
-set <- c(1, 2, 6, 10)
-for(i in set) {
-  attach(mtcars)    
-  par(mfrow=c(1,2))   # two plots together
-  n <- i
-  data.vec <- rnorm(SRS*n,mean=0,sd=1)
+i=0
+while(i < 30) {
+  data.vec <- rnorm(40,mean=10,sd=2)
   data.mat <- matrix(data.vec, ncol = n)
   avg <- apply(data.mat, 1, mean)
   
   m = mean(avg)
   std = sd(avg)
-  # print(n, m, std)
-  str = sprintf("n = %d, mean = %f, std = %f, sample std = %f", n,m, std, std*sqrt(n))
-  print(str)
-  
-  # windows()#this is optional
-  hist(avg, xlab="Data from Normal Distribution", freq = FALSE, main=sprintf("Histogram for Normal, n = %d",n))
-  curve(dnorm(x, mean=m, sd=std), col="blue", lwd=2, add=TRUE)
-  lines(density(avg, adjust=1),col = "red", lwd=2)
-  # windows()
-  # quartz()
-  qqnorm(avg,main=sprintf("Normal Quantile Plot for Normal, n = %d",n))
-  qqline(avg)
-  quartz()
+  print(z.test(data.vec, conf.level = 0.80, alternative="two.sided", sd=std))
+  i<- i+1
 }
+
+# B
+hogs=read.table(file="hogs.txt",header=T)
+
+print(hogs$Weight.lb.)
+quartz()
+hist(hogs$Weight.lb.,freq = FALSE)          # frequency should be false to get density curve.....
+means <- mean(hogs$Weight.lb.)
+print(means)
+std <- sd(hogs$Weight.lb.)
+curve(dnorm(x, mean=means, sd=std), col="blue", lwd=2, add=TRUE)   # normal distribution line
+lines(density(hogs$Weight.lb., adjust=2),col = "red", lwd=2)
+mean(hogs$Weight.lb.) 
+sd(hogs$Weight.lb.)
+
+# boxplot
+quartz()
+boxplot(hogs$Weight.lb.)
+points(means, pch = 18)
+
+
+
+#  b) Make a Normal quantile plot to confirm that there are no systematic departures from Normality.
+quartz()
+qqnorm(hogs$Weight.lb.,main="Normal Quantile Plot for normal distribution") 
+qqline(hogs$Weight.lb.)
+
+t.test(hogs$Weight.lb., conf.level=0.95, alternative = "two.sided")
+
+stdev = sd(hogs$Weight.lb.)
+stdev
+z.test(hogs$Weight.lb., conf.level = 0.95, alternative="two.sided", sd=stdev)
+
+
+
+
+
+
+
+
+
 
 
 
