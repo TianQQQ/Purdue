9eb4a5488b1c3dcd98500dbc2426ccdd82714dfd
Tian Qiu <qiu58@purdue.edu>
2016-03-20 21:23:57 -0400

    stack test

--- STAT598_FinancialAlgorithms_C++/HW3/HW3/HW3/Stack.cpp
+++ STAT598_FinancialAlgorithms_C++/HW3/HW3/HW3/Stack.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+
+
+/*一）、类模板的定义
+ template  <类型形参表>
+ class  <类名>
+ {     //类说明体  }；
+ template  <类型形参表>
+ <返回类型> <类名> <类型名表>::<成员函数1>（形参表）
+ {     //成员函数定义体  }
+ template  <类型形参表>
+ <返回类型> <类名> <类型名表>::<成员函数2>（形参表）
+ {     //成员函数定义体  }
+ …
+ template  <类型形参表>
+ <返回类型> <类名> <类型名表>::<成员函数n>（形参表）
+ {     //成员函数定义体  }*/
+/*
+template<class T>
+struct stack_node{
+    T next;
+    T val;
+    stack node(T∗ next) : next( next) {}
+};*/
