1482303d7b4fc5be9858256b4fff4bbb9d72133e
Tian Qiu <qiu58@purdue.edu>
2016-03-24 14:07:00 -0400

    lab6 initialization

--- STAT350_R/STAT350/Labs/Lab6/lab6test.R
+++ STAT350_R/STAT350/Labs/Lab6/lab6test.R
@@ -1,3 +1,13 @@
 yoga=read.table(file="yoga.txt",header=T,sep="\t") 
 attach(yoga)
 
+# The code for parts a) and c) are omitted. Please see # Labs 2 (boxplot) and 3 (histogram) for details.
+# Transformation
+logMin = log(Time..min.)
+
+
+#t.test(logMin, conf.level=0.95, mu = 1.4, alternative = "greater")
+#You always indicate confidence level, alpha = 1 ??? C
+#possibilities for alternative are "two.sided" (confidence interval),
+#"less" (upper confidence bound for one-sided test), "greater" (lower confidence bound for one-sided test)
+t.test(logMin, conf.level=0.95, mu = 1.4, alternative = "greater")
