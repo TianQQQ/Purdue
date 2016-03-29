d294f1f317d1f7de535cb457c91938c45dbdddbd
Tian Qiu <qiu58@purdue.edu>
2016-03-29 19:17:51 -0400

    stat lab7 initialization

---
+++ STAT350_R/STAT350/Labs/Lab7/T-Procedures-for Matched-Pairs.R
@@ -0,0 +1,19 @@
+setwd("~/Desktop/Purdue/STAT350_R/STAT350/Labs/Lab7")
+mpg=read.table(file="ex07-39mpgdiff.txt",header=T) 
+attach (mpg)
+# For the diagnoistics plots, you will need to create the one sample # data which is the difference between the two sets. You will need to # create the histogram, boxplot and QQPlot on this data set
+# (code not included)
+normaltest = Driver - Computer
+#t.test (x,y,conf.level=C, mu = mu0, paired=TRUE, alternative="value") # is used for confidence intervals and hypothesis tests
+# conf.level = C = 1 - alpha
+# for the hypothesis test. mu is mu_0
+#  paired = TRUE (2-sample paired)
+# The pairing will be x ??? y
+# alternative = "greater" or "less" or "two.sided" (this is the
+#  for two-sample independent ONLY appropriate alternative hypothesis)
+t.test(Driver,Computer, mu=0, alternative = "two.sided")
+# Information required for f
+std = sd(normaltest)
+size = length(Driver)
+se = std/sqrt(size)
+
