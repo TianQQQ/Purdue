e6e72e7795b3f87de52ce26ae03f79c8f0959da7
Tian Qiu <qiu58@purdue.edu>
2016-03-31 23:40:45 -0400

    lan7 bonus part left

--- STAT350_R/STAT350/Labs/Lab7/Lab7.R
+++ STAT350_R/STAT350/Labs/Lab7/Lab7.R
@@ -4,6 +4,7 @@ attach (mpg)
 # For the diagnoistics plots, you will need to create the one sample # data which is the difference between the two sets. You will need to # create the histogram, boxplot and QQPlot on this data set
 # (code not included)
 normaltest = avgTaxiOut - avgTaxiIn
+histogram(avgTaxiOut - avgTaxiIn)
 #t.test (x,y,conf.level=C, mu = mu0, paired=TRUE, alternative="value") # is used for confidence intervals and hypothesis tests
 # conf.level = C = 1 - alpha
 # for the hypothesis test. mu is mu_0
@@ -11,9 +12,14 @@ normaltest = avgTaxiOut - avgTaxiIn
 # The pairing will be x ??? y
 # alternative = "greater" or "less" or "two.sided" (this is the
 #  for two-sample independent ONLY appropriate alternative hypothesis)
-t.test(avgTaxiOut,avgTaxiIn, mu=0,conf.level=0.95,paired = TRUE, alternative = "two.sided")
+t.test(avgTaxiOut,avgTaxiIn, mu=10,conf.level=0.99,paired = TRUE, alternative = "greater")
 # Information required for f
 std = sd(normaltest)
 size = length(avgTaxiOut)
 se = std/sqrt(size)
 
+
+CHICAGO=read.table(file="airline_cleaned.txt",header=T)
+CHICAGO$TaxiIn
+avgTaxiIn <- subset(CHICAGO, Origin=="avgTaxiIn")
+attach(avgTaxiOut)
--- STAT350_R/STAT350/Labs/Lab7/Lab7_indep.R
+++ STAT350_R/STAT350/Labs/Lab7/Lab7_indep.R
@@ -1,12 +1,16 @@
 setwd("~/Desktop/Purdue/STAT350_R/STAT350/Labs/Lab7")
 CHICAGO=read.table(file="airline_cleaned.txt",header=T)
 studynew <- subset(CHICAGO, Origin=="ORD" | Origin == "MDW")
+ord <- subset(CHICAGO, Origin=="ORD")
+mdw <- subset(CHICAGO, Origin=="MDW")
 
 attach(studynew)
 # Note: it is required that you have two curves (red and blue) on the
 # histogram and the line on the normal quantile plot as done by the
 #   code below.
 library(lattice)
+histogram(log(abs(ord$DepDelay)))
+histogram(log(abs(mdw$DepDelay)))
 histogram(~log(abs(DepDelay)) | Origin, layout=c(1,2),type="density", panel=function(x)
 {panel.histogram(x) 
   panel.mathdensity(dmath=dnorm,col="blue",lwd=2,args=list(mean=mean(x, na.rm=T), sd = sd(x,na.rm=T))) 
@@ -31,7 +35,7 @@ qqmath(~log(abs(DepDelay))| Origin, data = studynew, panel = function(x){
 #var.equal = FALSE (the variances are not equal, R calls
 #                   the Satterthwaite approximation the Welch approximation)
 
-t.test(DepDelay ~ Origin, studynew, mu=0, conf.level=0.99,paired=FALSE, alternative = "less",var.equal=F)
+t.test(DepDelay ~ Origin, studynew, mu=0, conf.level=0.95,paired=FALSE, alternative = "two.sided",var.equal=F)
 # Information required for f
 stdmen = sd(subset(studynew,Origin == "ORD")$DepDelay)
 sizemen = length(subset(studynew,Origin == "ORD")$DepDelay) 
