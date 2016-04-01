449ed6b37dad90bbde554704e9244332b06dfaa1
Tian Qiu <qiu58@purdue.edu>
2016-03-31 22:40:07 -0400

    delay error fixed

--- STAT350_R/STAT350/Labs/Lab7/Lab7_indep.R
+++ STAT350_R/STAT350/Labs/Lab7/Lab7_indep.R
@@ -1,29 +1,41 @@
-study=read.table(file="studyhabits.txt",header=T)
-studynew <- subset(study, Sex=="Men" | Sex == "Women")
+setwd("~/Desktop/Purdue/STAT350_R/STAT350/Labs/Lab7")
+CHICAGO=read.table(file="airline_cleaned.txt",header=T)
+studynew <- subset(CHICAGO, Origin=="ORD" | Origin == "MDW")
 
 attach(studynew)
 # Note: it is required that you have two curves (red and blue) on the
 # histogram and the line on the normal quantile plot as done by the
 #   code below.
 library(lattice)
-histogram(~SSHA | Sex, layout=c(1,2),type="density", panel=function(x)
-{
-  panel.histogram(x) 
+histogram(~log(abs(DepDelay)) | Origin, layout=c(1,2),type="density", panel=function(x)
+{panel.histogram(x) 
   panel.mathdensity(dmath=dnorm,col="blue",lwd=2,args=list(mean=mean(x, na.rm=T), sd = sd(x,na.rm=T))) 
   panel.densityplot(x,col="red",lwd=2)
-})
-bwplot(~SSHA | Sex, layout = c(1, 2), pch = "|") #Boxplots side-by-side 
-qqmath(~SSHA| Sex, data = study, panel = function(x){
+  })
+bwplot(~log(abs(DepDelay)) | Origin, layout = c(1, 2), pch = "|") #Boxplots side-by-side 
+qqmath(~log(abs(DepDelay))| Origin, data = studynew, panel = function(x){
   panel.qqmath(x)
   panel.qqmathline(x)
 })
 
+# t test:
+#t.test (qual ~ categories,conf.level=C, mu = mu0, paired=FALSE,
+#alternative="value", var.equal = FALSE)
+#is used for confidence intervals and hypothesis tests
+#the qualitative variable is first, the variable with the groups in it is second.
+#The difference is in first alphabetically ??? second alphabetically
+#  conf.level = C = 1 - alpha
+#  for the hypothesis test. mu is mu_0
+#  paired = FALSE (2 - sample independent)
+#alternative = "greater" or "less" or "two.sided" (this is the appropriate alternative hypothesis)
+#var.equal = FALSE (the variances are not equal, R calls
+#                   the Satterthwaite approximation the Welch approximation)
 
-t.test(SSHA ~ Sex, study, mu=0, conf.level=0.99,paired=FALSE, alternative = "less",var.equal=F)
+t.test(DepDelay ~ Origin, studynew, mu=0, conf.level=0.99,paired=FALSE, alternative = "less",var.equal=F)
 # Information required for f
-stdmen = sd(subset(study,Sex == "Men")$SSHA)
-sizemen = length(subset(study,Sex == "Men")$SSHA) 
-stdwomen = sd(subset(study,Sex == "Women")$SSHA) 
-sizewomen = length(subset(study,Sex == "Women")$SSHA) 
+stdmen = sd(subset(studynew,Origin == "ORD")$DepDelay)
+sizemen = length(subset(studynew,Origin == "ORD")$DepDelay) 
+stdwomen = sd(subset(studynew,Origin == "MDW")$DepDelay) 
+sizewomen = length(subset(studynew,Origin == "MDW")$DepDelay) 
 se = sqrt(stdmen^2/sizemen + stdwomen^2/sizewomen)
 
