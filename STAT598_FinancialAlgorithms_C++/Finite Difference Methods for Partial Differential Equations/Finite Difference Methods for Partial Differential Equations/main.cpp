9bee052a993614796134d3f3ac96cdee62d5181b
Tian Qiu <qiu58@purdue.edu>
2016-04-10 17:33:34 -0400

    Black-Scholes Model initialization

---
+++ STAT598_FinancialAlgorithms_C++/Finite Difference Methods for Partial Differential Equations/Finite Difference Methods for Partial Differential Equations/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Finite Difference Methods for Partial Differential Equations
+//
+//  Created by Tian Qiu on 16/4/10.
+//  Copyright © 2016年 Tian Qiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ STAT598_FinancialAlgorithms_C++/Option Pricing Via Binomial Tree/Option Pricing Via Binomial Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Option Pricing Via Binomial Tree
+//
+//  Created by Tian Qiu on 16/4/10.
+//  Copyright © 2016年 Tian Qiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
